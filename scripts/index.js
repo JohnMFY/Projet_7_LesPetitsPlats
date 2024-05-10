a95fa476b772690c4de670d5bb965dd2e5e6f265
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2024-05-10 16:49:21 +0200

    Style of cards

--- scripts/index.js
+++ scripts/index.js
@@ -58,66 +58,71 @@
     //onClick on the cross(deleteOption) = selectedOption.pull(array, XXXX)
 
   /************************/
- /**** TEMPLATE RECIPES **/
+ /*** TEMPLATE RECIPES ***/
 /************************/
-function recipeCard() {
-    const recipePicture = `assets/recipes/${recipes[i].image}`;
-    const recipesSection = document.getElementById('recipesSection');
-    const article = document.createElement( 'article' );
-    article.setAttribute('class', 'recipes_card');
+    function recipeCard() {
+        const recipePicture = `assets/recipes/${recipes[i].image}`;
+        const recipesSection = document.getElementById('recipesSection');
+        const article = document.createElement( 'article' );
+        article.setAttribute('class', 'recipes_card');
 
-    const img = document.createElement('img');
-    img.setAttribute('src', recipePicture);
-    img.setAttribute('class', 'recipePicture');
+        const img = document.createElement('img');
+        img.setAttribute('src', recipePicture);
+        img.setAttribute('class', 'recipePicture');
 
-    const h4 = document.createElement('h4');
-    h4.textContent = recipes[i].name;
+        const time = document.createElement('p')
+        time.setAttribute('class', 'time')
+        time.textContent = recipes[i].time +'min'
 
-    const h5Recette = document.createElement('h5');
-    h5Recette.textContent = 'RECETTE';
+        const h4 = document.createElement('h4');
+        h4.textContent = recipes[i].name;
 
-    const p = document.createElement('p');
-    p.textContent = recipes[i].description;
+        const h5Recette = document.createElement('h5');
+        h5Recette.textContent = 'RECETTE';
 
-    const h5Ingredients = document.createElement('h5');
-    h5Ingredients.textContent = 'INGREDIENTS';
+        const p = document.createElement('p');
+        p.setAttribute('class','recipeDescription')
+        p.textContent = recipes[i].description;
 
-    const divIngredients = document.createElement('div');
-    divIngredients.setAttribute('class', 'ingredients');
-    
-    //boucle for ingredients
+        const h5Ingredients = document.createElement('h5');
+        h5Ingredients.textContent = 'INGREDIENTS';
 
-    let recipeIngredients = recipes[i].ingredients;
+        const divIngredients = document.createElement('div');
+        divIngredients.setAttribute('class', 'ingredients');
+        
+        //boucle for ingredients//
+            let recipeIngredients = recipes[i].ingredients;
 
-    recipeIngredients.forEach((ingredients) =>{
-        const divIngredient = document.createElement('div');
-        divIngredient.setAttribute('class', 'ingredient');
+            recipeIngredients.forEach((ingredients) =>{
+                const divIngredient = document.createElement('div');
+                divIngredient.setAttribute('class', 'ingredient');
 
-        let ingredient = document.createElement('p');
-        ingredient.textContent = ingredients.ingredient;
+                let ingredient = document.createElement('p');
+                ingredient.textContent = ingredients.ingredient;
 
-        const divQtyUnit = document.createElement('div');
-        let quantity = document.createElement('p');
-        quantity.textContent = ingredients.quantity;
-        let unit = document.createElement('p');
-        unit.textContent = ingredients.unit;
+                const divQtyUnit = document.createElement('div');
+                divQtyUnit.setAttribute('class', 'quantityAndUnit')
+                let quantity = document.createElement('p');
+                quantity.textContent = ingredients.quantity;
+                let unit = document.createElement('p');
+                unit.textContent = ingredients.unit;
 
-        divIngredient.appendChild(ingredient);
-        divIngredient.appendChild(divQtyUnit);
-        divQtyUnit.appendChild(quantity);
-        divQtyUnit.appendChild(unit);
-        divIngredients.appendChild(divIngredient);
-    })
-    
-    article.appendChild(img);
-    article.appendChild(h4);
-    article.appendChild(h5Recette);
-    article.appendChild(p);
-    article.appendChild(h5Ingredients);
-    article.appendChild(divIngredients);
-    recipesSection.appendChild(article);
-
-}
+                divIngredient.appendChild(ingredient);
+                divIngredient.appendChild(divQtyUnit);
+                divQtyUnit.appendChild(quantity);
+                divQtyUnit.appendChild(unit);
+                divIngredients.appendChild(divIngredient);
+            })
+        //
+        article.appendChild(img);
+        article.appendChild(time)
+        article.appendChild(h4);
+        article.appendChild(h5Recette);
+        article.appendChild(p);
+        article.appendChild(h5Ingredients);
+        article.appendChild(divIngredients);
+        recipesSection.appendChild(article);
+    }
 /****************************************************************************/    
 /******************* INTEGRATION OF RECIPES IN THE DOM *********************/
 /**************************************************************************/
@@ -128,6 +133,7 @@ function recipeCard() {
     for( i = 0 ; i < ingredientsArray.length; i++){
         const optionIngredient = document.createElement('option');
         optionIngredient.setAttribute('value', ingredientsArray[i]);
+        optionIngredient.setAttribute('class', 'ingredientsOption')
         optionIngredient.textContent = ingredientsArray[i];
         ingredientsSelect.appendChild(optionIngredient)
     }
@@ -136,6 +142,7 @@ function recipeCard() {
     for( i = 0 ; i < appareilArray.length; i++){
         const optionAppareil = document.createElement('option');
         optionAppareil.setAttribute('value', appareilArray[i]);
+        optionAppareil.setAttribute('class', 'appareilsOption')
         optionAppareil.textContent = appareilArray[i];
         appareilsSelect.appendChild(optionAppareil)
     }
@@ -145,10 +152,52 @@ function recipeCard() {
     for( i = 0 ; i < ustensilsArray.length; i++){
         const optionUstensil = document.createElement('option');
         optionUstensil.setAttribute('value', ustensilsArray[i]);
+        optionUstensil.setAttribute('class', 'ustensilsOption')
         optionUstensil.textContent = ustensilsArray[i];
         ustensilesSelect.appendChild(optionUstensil)
     }
+
 /**** RECIPES CARDS INTEGRATION ****/
 for(i = 0; i < recipes.length; i++){
     recipeCard()
 }
+
+  /*******************************/
+ /** FUNCTIONS OF DYNAMISATION **/
+/*******************************/
+
+    /**** SELECTION OF OPTION ****/
+
+        const selectedOptionDiv = document.getElementById('selectedItems') //div for DOM insertion
+
+        /** SELECTION OF INGREDIENT **/
+        function selectedOptionIngredients(){
+            let ingredientsOption = document.querySelectorAll('.ingredientsOption')
+            ingredientsOption.forEach(ingredientOption =>{
+                //console.log(ingredientOption);
+                ingredientOption.addEventListener('click', () => {
+                    console.log('ok ingredient option ' + ingredientOption.value)
+                });
+            })
+        }selectedOptionIngredients()
+        /** SELECTION OF APPAREILS **/
+        function selectedOptionAppareils(){
+            let appareilsOption = document.querySelectorAll('.appareilsOption')
+            appareilsOption.forEach(appareilOption =>{
+                //console.log(appareilOption);
+                appareilOption.addEventListener('click', () => {
+                    console.log('ok appareil option '+ appareilOption.value)
+                });
+            })
+        }selectedOptionAppareils()
+        /** SELECTION OF USTENSILS **/
+        function selectedOptionUstensils(){
+            let ustensilsOption = document.querySelectorAll('.ustensilsOption')
+            ustensilsOption.forEach(ustensilOption =>{
+                //console.log(appareilOption);
+                ustensilOption.addEventListener('click', () => {
+                    console.log('ok appareil option '+ ustensilOption.value)
+                });
+            })
+        }selectedOptionUstensils()
+    /*****************************/
